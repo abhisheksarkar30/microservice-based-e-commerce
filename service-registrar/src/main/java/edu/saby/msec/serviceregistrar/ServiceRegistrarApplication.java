2b3ef79c80343ad693044a7e1f22a63c83e60ab3
abhisheksarkar30 <8725485+abhisheksarkar30@users.noreply.github.com>
2020-08-29 23:19:21 +0530

    Initial draft of service-registrar

---
+++ service-registrar/src/main/java/edu/saby/msec/serviceregistrar/ServiceRegistrarApplication.java
@@ -0,0 +1,15 @@
+package edu.saby.msec.serviceregistrar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistrarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistrarApplication.class, args);
+	}
+
+}
---
+++ service-registrar/src/test/java/edu/saby/msec/serviceregistrar/ServiceRegistrarApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.saby.msec.serviceregistrar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistrarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
