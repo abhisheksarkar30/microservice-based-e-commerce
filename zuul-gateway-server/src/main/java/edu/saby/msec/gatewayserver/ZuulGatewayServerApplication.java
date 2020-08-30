44af495b941d514875b2e1f8dc04fcf9e515e0bf
abhisheksarkar30 <8725485+abhisheksarkar30@users.noreply.github.com>
2020-08-30 12:21:07 +0530

    Initial draft of zuul-gateway

---
+++ zuul-gateway-server/src/main/java/edu/saby/msec/gatewayserver/ZuulGatewayServerApplication.java
@@ -0,0 +1,17 @@
+package edu.saby.msec.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ZuulGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayServerApplication.class, args);
+	}
+
+}
---
+++ zuul-gateway-server/src/test/java/edu/saby/msec/gatewayserver/ZuulGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.saby.msec.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
