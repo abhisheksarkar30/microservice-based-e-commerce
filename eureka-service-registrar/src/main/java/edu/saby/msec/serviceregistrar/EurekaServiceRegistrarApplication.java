d3a14b909995e225edbcd1b9a1da7a9ed9f8ce88
abhisheksarkar30 <8725485+abhisheksarkar30@users.noreply.github.com>
2020-08-30 09:36:02 +0530

    service-registrar refactors

--- eureka-service-registrar/src/main/java/edu/saby/msec/serviceregistrar/EurekaServiceRegistrarApplication.java
+++ eureka-service-registrar/src/main/java/edu/saby/msec/serviceregistrar/EurekaServiceRegistrarApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ServiceRegistrarApplication {
+public class EurekaServiceRegistrarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceRegistrarApplication.class, args);
+		SpringApplication.run(EurekaServiceRegistrarApplication.class, args);
 	}
 
 }
